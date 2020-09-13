2dbca0c890c22a1ed5a62465e0b566372ced11de
Matthew Bell <matthewbell1030@gmail.com>
2020-09-12 17:23:50 -0700

    updated page title and removed boilerplate comments in index.html. updated dependencies in app.js

--- src/App.js
+++ src/App.js
@@ -16,58 +16,61 @@ import ForecastCard from "./components/ForecastCard";
 class App extends Component {
   render() {
     return (
-      <MDBContainer style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-        <MDBRow center>
-          <MDBCol middle="true" sm="8" className="text-center">
-            <Header></Header>
-            {/* Main */}
-            {/* main row 1 - Search and ForecastHeader row */}
-            <MDBRow>
-              <MDBCol>
-                <SearchCard></SearchCard>
-              </MDBCol>
-              <MDBCol>
-                <ForecastHeader></ForecastHeader>
-              </MDBCol>
-            </MDBRow>
-            {/* End Search and ForecastHeader row */}
+      
+        <MDBContainer style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+          <MDBRow center>
+            <MDBCol middle="true" sm="8" className="text-center">
+              <Header></Header>
+              {/* Main */}
+              <main style={{ flexGrow: "1", textAlign: "center", padding: "25px" }}>
+                {/* main row 1 - Search and ForecastHeader row */}
+                <MDBRow>
+                  <MDBCol>
+                    <SearchCard></SearchCard>
+                  </MDBCol>
+                  <MDBCol>
+                    <ForecastHeader></ForecastHeader>
+                  </MDBCol>
+                </MDBRow>
+                {/* End Search and ForecastHeader row */}
 
-            {/* main row 2 - PreviousSearches and ForecastGraphic row */}
-            <MDBRow>
-              <MDBCol>
-                <PreviousSearches></PreviousSearches>
-              </MDBCol>
-              <MDBCol>
-                <ForecastGraphic></ForecastGraphic>
-              </MDBCol>
-            </MDBRow>
-            {/* End PreviousSearches and ForecastGraphic row */}
+                {/* main row 2 - PreviousSearches and ForecastGraphic row */}
+                <MDBRow>
+                  <MDBCol>
+                    <PreviousSearches></PreviousSearches>
+                  </MDBCol>
+                  <MDBCol>
+                    <ForecastGraphic></ForecastGraphic>
+                  </MDBCol>
+                </MDBRow>
+                {/* End PreviousSearches and ForecastGraphic row */}
 
-            <h4>5-day Forecast</h4>
-            {/* main row 3 - ForecastCards(x5) row */}
-            <MDBRow>
-              <MDBCol>
-                <ForecastCard></ForecastCard>
-              </MDBCol>
-              <MDBCol>
-                <ForecastCard></ForecastCard>
-              </MDBCol>
-              <MDBCol>
-                <ForecastCard></ForecastCard>
-              </MDBCol>
-              <MDBCol>
-                <ForecastCard></ForecastCard>
-              </MDBCol>
-              <MDBCol>
-                <ForecastCard></ForecastCard>
-              </MDBCol>
-            </MDBRow>
-            {/* End ForecastCards row */}
-            {/* End Main */}
-            <Footer></Footer>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+                <h4>5-day Forecast</h4>
+                {/* main row 3 - ForecastCards(x5) row */}
+                <MDBRow>
+                  <MDBCol>
+                    <ForecastCard></ForecastCard>
+                  </MDBCol>
+                  <MDBCol>
+                    <ForecastCard></ForecastCard>
+                  </MDBCol>
+                  <MDBCol>
+                    <ForecastCard></ForecastCard>
+                  </MDBCol>
+                  <MDBCol>
+                    <ForecastCard></ForecastCard>
+                  </MDBCol>
+                  <MDBCol>
+                    <ForecastCard></ForecastCard>
+                  </MDBCol>
+                </MDBRow>
+                {/* End ForecastCards row */}
+              </main>
+              {/* End Main */}
+              <Footer></Footer>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
     );
   }
 }
