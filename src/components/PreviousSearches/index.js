3a80a3f10ea6591f4272f53948b6c59612614d9f
Matthew Bell <matthewbell1030@gmail.com>
2020-09-12 18:38:16 -0700

    updated format and positioning of PreviousSearches component.  added list to PreviousSearches component, including 3 mdb-react dependencies.

--- src/components/PreviousSearches/index.js
+++ src/components/PreviousSearches/index.js
@@ -1,14 +1,25 @@
 //Dependencies
 import React from "react";
-import {MDBRow, MDBCol } from "mdbreact";
+import { MDBRow, MDBCol, MDBListGroup, MDBListGroupItem } from "mdbreact";
 //Component
 function PreviousSearches () {
-      return (
-          <MDBRow className="mt-5 mb-3">
-            <MDBCol>
-              <h5>Previous Searches</h5>
-            </MDBCol>
-          </MDBRow>
-      );
-  }
+  return (
+    <MDBCol className="mt-5 mb-3">
+      <MDBRow>
+        <MDBCol className="text-center">
+          <h5>
+            Previous Searches
+          </h5>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow>
+          <MDBListGroup style={{ width:"22rem" }}>
+            <MDBListGroupItem>Placeholder 1</MDBListGroupItem>
+            <MDBListGroupItem>Placeholder 2</MDBListGroupItem>
+            <MDBListGroupItem>Placeholder 3</MDBListGroupItem>
+          </MDBListGroup>
+      </MDBRow>
+    </MDBCol>
+  );
+}
 export default PreviousSearches;
