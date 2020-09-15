12a08c58adc6cfddcc0979e8fb1b40a5c577b4ae
Matthew Bell <matthewbell1030@gmail.com>
2020-09-14 19:37:25 -0700

    updated padding for SearchCard component.

--- src/components/SearchCard/index.js
+++ src/components/SearchCard/index.js
@@ -7,7 +7,7 @@ function SearchCard () {
         <MDBCol className="mt-5 mb-3" style={{ minHeight: "60px", background:"#57bd74" }}>
           <MDBFormInline className="md-form mr-auto mb-4">
             <MDBRow>
-              <MDBCol sm="6" md="6" lg="6" xl="8" className="mr-2">
+              <MDBCol sm="6" md="6" lg="6" xl="8" className="mr-2 p-1">
                 <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
               </MDBCol>
               <MDBCol sm="2" md="2" lg="2" xl="3" className="ml-2">
