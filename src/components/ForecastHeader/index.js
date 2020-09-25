7f9992d6c2956a81503a11539473a018c991389e
Matthew Bell <matthewbell1030@gmail.com>
2020-09-24 23:04:24 -0700

    updated ForecastHeader component to use imported 'getForecast' function.

--- src/components/ForecastHeader/index.js
+++ src/components/ForecastHeader/index.js
@@ -9,25 +9,7 @@ function ForecastHeader () {
   //API Call
   let [responseObj, setResponseObj] = useState({});
 
-  // function getForecast() {
-  //   fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=seattle", {
-  //     "method": "GET",
-  //     "headers": {
-  //       "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-  //       "x-rapidapi-key": "**"
-	//     }
-  //   })
-  //   .then(response => response.json())
-  //   .then(response => {
-  //     setResponseObj(response)
-  //   })
-  //   .then(response => {
-  //     console.log(response);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   });
-  // }
+  getForecast();
 
   //JSX
   return (
